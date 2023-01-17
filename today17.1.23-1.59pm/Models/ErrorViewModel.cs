bc19afb1715293d7a2c9e509a87fd92b6ba02c58
Prakashdat <prakashdata@gmail.com>
2023-01-17 14:25:56 +0530

    Add project files.

---
+++ today17.1.23-1.59pm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace today17._1._23_1._59pm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
